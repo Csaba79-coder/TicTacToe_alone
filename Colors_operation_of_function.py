fd2555df1a361371f7303d43abe6f44b7b18ec4c
Csaba79-coder <77699103+Csaba79-coder@users.noreply.github.com>
2021-03-19 14:47:08 +0100

    Create Colors_operation_of_function.py
    Create Colors_operation_of_function.py

---
+++ Colors_operation_of_function.py
@@ -0,0 +1,7 @@
+def print_MenuDesign():
+    print(f"{colors.FGRED} __     __         {colors.END}")
+    print(f"{colors.FGRED}|   |   /  __ \ |   /  /   \ |   |    |  |{colors.END}")
+    print(f"{colors.FGWHITE}  | |   | | | /  /   | |/ /\ \ /  /   | | | | | | | {colors.END} ")
+    print(f"{colors.FGWHITE}  | |   | | | |       | ||    | |       | | | | | |  |{colors.END} ")
+    print(f"{colors.FGGREEN}  | |  | || _/\   | || | | | _/\   | | \ _/ / |__ {colors.END}")
+    print(f"{colors.FGGREEN}  _/  _/ _/   _/_| |/___/   _/  _/__/ {colors.END}")
